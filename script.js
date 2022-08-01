d49f519bed5b8e3266c74100c33f7ec40579d074
Denis Rodrigues <denisrodrigues.ita@gmail.com>
2022-08-01 17:21:09 -0300

    criando modules no js

--- script.js
+++ script.js
@@ -61,3 +61,26 @@ function initScrollSuave() {
     $linksInternos.forEach(link => link.addEventListener('click', scrollToSection));
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+    const $sections = document.querySelectorAll('.js-scroll')
+    if ($sections.length) {
+        const windowMetade = window.innerHeight * 0.5;
+
+        function animaScroll() {
+            $sections.forEach(item => {
+                const sectionTop = item.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible) {
+                    item.classList.add('ativo');
+                } else { // Se o else estiver no código, quando voltamos, os itens somem
+                    item.classList.remove('ativo');
+                }
+            });
+        }
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+initAnimacaoScroll();
