45d890745882ebf5647fd9b4d0a231ff1317af44
Denis Rodrigues <denisrodrigues.ita@gmail.com>
2022-08-02 18:49:57 -0300

    dropdown-menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js"
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
