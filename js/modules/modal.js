fa8f96d4d4517896f3822f6d5689e40529c49c28
denis rodrigues <denisrodrigues.ita@gmail.com>
2022-08-23 20:01:09 -0300

    refatorando tooltip para class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -5,7 +5,7 @@ export default class Modal {
     this.containerModal = document.querySelector(container);
 
     // bind this ao callback para fazer referência ao objeto da classe.
-    this.eventToggleModal = this.ecentToggleModal.bind(this);
+    this.eventToggleModal = this.eventToggleModal.bind(this);
     this.cliqueForaModal = this.cliqueForaModal.bind(this);
   }
 
