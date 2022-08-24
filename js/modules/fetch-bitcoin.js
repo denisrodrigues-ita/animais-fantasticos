06b49822a264dc8bd3a8917e11b6bfd47606e190
denis rodrigues <denisrodrigues.ita@gmail.com>
2022-08-24 17:56:41 -0300

    refatorando fetch-bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,9 +1,9 @@
-export default function initFetchBitcoin() {
+export default function fetchBitcoin(url, target) {
   async function fetchbtc() {
     try {
-      const btcUrl = await fetch('https://blockchain.info/ticker');
+      const btcUrl = await fetch(url);
       const btcJson = await btcUrl.json();
-      const btcPreco = document.querySelector('.btc-preco');
+      const btcPreco = document.querySelector(target);
       btcPreco.innerText = (1000 / btcJson.BRL.sell).toFixed(4);
     } catch (erro) {
       console.log(erro);
